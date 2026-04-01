bee6b07ea52451c089b5956797de2f29471b63d9
alvarosasca04 <alvarosasca04@gmail.com>
2026-03-31 19:44:26 -0600

    Update yaxis parameter in plot_opex_comparison function

---
+++ path/to/your/file.py
@@ -0,0 +1,7 @@
+    # Updated the yaxis parameter as requested
+    yaxis=dict(
+        gridcolor=COLORS["border"],
+        gridwidth=0.5,
+        linecolor=COLORS["border"],
+        tickformat="$.2f"
+    )
